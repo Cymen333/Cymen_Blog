4e9e8fbda379d23734694233ef54704833ba83e7
Cymen333 <sn150884@163.com>
2026-05-20 20:56:06 +0800

    修正仓库路径配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   // 把你的 GitHub 用户名替换到下面
-  site: 'https://github.com/Cymen333/Cymen_Blog.git', 
+  site: 'https://Cymen333.github.io', 
   // 你的 GitHub 仓库名字（必须加斜杠）
-  base: '/my-blog-Astro', 
+  base: '/Cymen_Blog', 
 });
